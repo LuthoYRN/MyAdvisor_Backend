6aef4c832fd929db59e93d7a31cffa4ebc4996f9
Lutho <luthoyrn@gmail.com>
2024-08-16 04:02:14 +0200

    implemented member functions and static methods for advisorMajor junction class

--- Backend/models/advisorMajor.js
+++ Backend/models/advisorMajor.js
@@ -7,7 +7,41 @@ module.exports = (sequelize, DataTypes) => {
       AdvisorMajor.belongsTo(models.Advisor, { foreignKey: "advisorID" });
       AdvisorMajor.belongsTo(models.Major, { foreignKey: "majorID" });
     }
+
+    // Static method to find all majors for a given advisor
+    static async findByAdvisor(advisorID) {
+      try {
+        return await AdvisorMajor.findAll({
+          where: { advisorID },
+          include: [{ model: sequelize.models.Major }],
+        });
+      } catch (error) {
+        throw new Error("Error finding majors by advisor: " + error.message);
+      }
+    }
+
+    // Static method to find all advisors for a given major
+    static async findByMajor(majorID) {
+      try {
+        return await AdvisorMajor.findAll({
+          where: { majorID },
+          include: [{ model: sequelize.models.Advisor }],
+        });
+      } catch (error) {
+        throw new Error("Error finding advisors by major: " + error.message);
+      }
+    }
+
+    // Static method to create an AdvisorMajor association
+    static async createAdvisorMajor(advisorID, majorID) {
+      try {
+        return await AdvisorMajor.create({ advisorID, majorID });
+      } catch (error) {
+        throw new Error("Error creating AdvisorMajor: " + error.message);
+      }
+    }
   }
+
   AdvisorMajor.init(
     {
       id: {
@@ -17,16 +51,30 @@ module.exports = (sequelize, DataTypes) => {
       },
       advisorID: {
         type: DataTypes.INTEGER,
+        allowNull: false,
         references: {
           model: "advisors",
           key: "id",
         },
+        set(value) {
+          if (isNaN(value)) {
+            throw new Error("Invalid advisorID");
+          }
+          this.setDataValue("advisorID", value);
+        },
       },
       majorID: {
         type: DataTypes.INTEGER,
+        allowNull: false,
         references: {
           model: "majors",
           key: "id",
+        },
+        set(value) {
+          if (isNaN(value)) {
+            throw new Error("Invalid majorID");
+          }
+          this.setDataValue("majorID", value);
         },
       },
     },
@@ -36,5 +84,6 @@ module.exports = (sequelize, DataTypes) => {
       tableName: "advisors_majors",
     }
   );
+
   return AdvisorMajor;
 };
