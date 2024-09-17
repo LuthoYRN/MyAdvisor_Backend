a26468fe5fe1e0f25053c76926cface8f256187a
Lutho <luthoyrn@gmail.com>
2024-09-18 01:44:48 +0200

    Created systemAdmin model

---
+++ Backend/db/migrations/20240917234059-create-systemAdmin.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("systemAdmin", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      surname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true, // Ensures no duplicate emails
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("systemAdmin");
+  },
+};
