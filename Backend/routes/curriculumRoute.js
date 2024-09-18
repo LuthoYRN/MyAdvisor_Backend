fae723e5abca23bedfe6c4a3e8940de23fff2242
Lutho <luthoyrn@gmail.com>
2024-09-18 16:24:32 +0200

    Implemented AddExisting course API

--- Backend/routes/curriculumRoute.js
+++ Backend/routes/curriculumRoute.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const curriculumController = require("./../controllers/curriculumController");
+const courseController = require("./../controllers/courseController");
 const router = express.Router();
 //getting all courses by curriculum
 router
   .route("/:currID/courses")
   .get(curriculumController.getCoursesByCurriculum);
+//add existing course to curriculum
+router
+  .route("/:currID/courses/addExisting")
+  .get(courseController.getAllCourses);
 router
   .route("/:currID/courses/:courseID") //individual course list
   .get(curriculumController.checkIfSafeToDelete) //on delete click
   .delete(curriculumController.deleteCourseFromCurriculum); //on delete modal click
-//add existing course to curriculum
-router.route("/:currID/courses/addExisting").get();
+
 module.exports = router;
